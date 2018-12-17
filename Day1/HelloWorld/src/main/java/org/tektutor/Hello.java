ce9b2b27b8fa436e125e693d3cdd71b4357c1d3e
bhargav <bhargav.nanu@gmail.com>
2018-12-17 13:56:15 +0530

    Intial commit.

---
+++ Day1/HelloWorld/src/main/java/org/tektutor/Hello.java
@@ -0,0 +1,16 @@
+package org.tektutor;
+
+public class Hello {
+
+	public String sayHello(){
+		return "Hello Java!";
+	}
+
+	public static void main ( String[] args ) {
+
+		Hello obj = new Hello();
+
+		System.out.println  ( obj.sayHello() );
+	}
+
+}	
